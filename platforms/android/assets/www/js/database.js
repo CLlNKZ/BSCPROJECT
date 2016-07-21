0dcd6dff1a1578c054d0f162f87b82a5f6766497
CLlNKZ <thpogani@edu.aau.at>
2016-07-21 14:53:37 +0200

    Android build

--- platforms/android/assets/www/js/database.js
+++ platforms/android/assets/www/js/database.js
@@ -1,21 +1,71 @@
-var myDB = window.sqlitePlugin.openDatabase({name: "database.db", location: 'default'});
+window.alert("Start");
 
+var db;
+var shortName = 'DoubleCardOndbDB';
+var version = '1.0';
+var displayName = 'DoubleCardOndbDB';
+var maxSize = 8192;
+
+window.alert("Variables");
+
+// Wait for device API libraries to load
+//
+function onLoad() {
+	window.alert("Inload");
+    document.addEventListener("deviceready", onDeviceReady, false);
+}
+window.alert("Ready");
+
+
+function onDeviceReady() {
+	window.alert("DVReady");
+    var db = window.sqlitePlugin.openDatabase(shortName, version, displayName, maxSize);
+    db.transaction(populateDB, errorCB, successCB);
+}
+
+// this is called when an error happens in a transaction
+function errorHandler(transaction, error) {
+   alert('Error: ' + error.message + ' code: ' + error.code);
+
+}
+
+// this is called when a successful transaction happens
+function successCallBack() {
+   alert("DEBUGGING: success");
+
+}
+
+function nullHandler(){};
+
+db.transaction(function(tx){
+
+   tx.executeSql( 'CREATE TABLE IF NOT EXISTS Statistik(Spiel_ID INTEGER PRIMAR KEY, Spieler TEXT NOT NULL, Datum TEXT NOT NULL, Versuche INTEGER NOT NULL, Kartenset TEXT NOT NULL)',
+[],nullHandler,errorHandler);
+ },errorHandler,successCallBack);
+
+
+
+
+/*var myDB = window.sqlitePlugin.openDatabase({name: "database.db", location: 'default'});
+window.alert("After DB");
 myDB.transaction(function(transaction) {
 transaction.executeSql('CREATE TABLE IF NOT EXISTS statistik (Spiel_ID integer primary key, Spieler text, Datum text, Versuche integer, Kartenset string)', [],
 function(tx, result) {
-alert("Table created successfully");
+window.alert("Table created successfully");
 },
 function(error) {
-alert("Error occurred while creating the table.");
+window.alert("Error occurred while creating the table.");
 });
 });
 
 myDB.transaction(function(transaction) {
 transaction.executeSql('CREATE TABLE IF NOT EXISTS spieler (Spieler_ID integer primary key, name text)', [],
 function(tx, result) {
-alert("Table created successfully");
+window.alert("Table created successfully");
 },
 function(error) {
-alert("Error occurred while creating the table.");
+window.alert("Error occurred while creating the table.");
 });
 });
+
+window.alert("Worked");*/
