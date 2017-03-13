79605503f3ed346935134fc3db8764a02133764a
CLlNKZ <thpogani@edu.aau.at>
2017-03-13 15:09:20 +0100

    Index Files
    deviceready event

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -8,14 +8,14 @@ var app = {
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
+    bindEvents: function() { alert("bindEvents");
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
+    onDeviceReady: function() { alert("device");
         app.receivedEvent('deviceready');
 		
     },
