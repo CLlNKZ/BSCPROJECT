6610678e2e97fdc3910d72915004594bb0716703
SonjaWallner <Sonja_DanielaW@gmx.at>
2017-03-16 15:22:41 +0100

    DB_Changes_GetData()_Aufruf

--- www/js/einstellungen.js
+++ www/js/einstellungen.js
@@ -1,29 +1,29 @@
 //Add or Replace rows in PermStatistik
-addItemPermStatistik(1, Früchte, 4);
+//addItemPermStatistik(1, Früchte, 4);
 
 //Add rows to Statistik
-addItemStatistik(1, 1, Werkzeug, 3);
+//addItemStatistik(1, 1, Werkzeug, 3);
 
 //Update rows Einstellungen
-updateItemEinstellungen(2, Werkzeug);
+//updateItemEinstellungen(2, Werkzeug);
 
 //Update rows SpielerStatus
-updateItemSpielerStatus(1, Autos, 0, 20, 1);
+//updateItemSpielerStatus(1, Autos, 0, 20, 1);
 
 //Read Einstellungen
 getDataEinstellungen();
 
 //Read Statistik
-getDataStatistik(1, Werkzeug);
+//getDataStatistik(1, Werkzeug);
 
 //Read PermStatistik
-getDataPermStatistik(1, Früchte);
+//getDataPermStatistik(1, Früchte);
 
 //Read SpielerStatus
-getDataStatus(1);
+//getDataStatus(1);
 
 //Delete Statistik
 //deleteItemsStatistik()
 
 //Close DB
-closeDB();
+//closeDB();
