7db87275056da5d9e6e0c01141324d79ee6d833b
SonjaWallner <Sonja_DanielaW@gmx.at>
2017-03-23 16:45:39 +0100

    richtige anzeige der einstellungen

--- www/js/einstellungen.js
+++ www/js/einstellungen.js
@@ -1,11 +1,20 @@
 function storeCardSets(){
 	var myObj, myJSON, obj;
 	//Einstellungen von HTML holen
-	var fruits = document.getElementById("mySelect").value;
+	var playset = document.getElementById("mySelect").value;
 	var store = document.getElementById("mySelect3").value;
 	
 	//JSON Code store Data
-	myObj = { "set":fruits, "name":store };
+	myObj = { "set":playset, "name":store };
 	myJSON = JSON.stringify(myObj);
 	localStorage.setItem("einstellungen", myJSON);
+}
+
+function loadEinstellungen(){
+	var einstellungen = localStorage.getItem("einstellungen");
+	obj = JSON.parse(einstellungen);
+	var setauswahl = obj.set;
+	var playernr = obj.name;
+	document.getElementById("mySelect").value = setauswahl;
+	document.getElementById("mySelect3").value = playernr;
 }
