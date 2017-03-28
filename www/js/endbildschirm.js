37bd7e195740b8198cf4961099b70aa64be11221
SonjaWallner <Sonja_DanielaW@gmx.at>
2017-03-28 20:09:17 +0200

    AusrichtungEndbildschirmBerichtigen+Versuche ausgeben und neu anordnen

--- www/js/endbildschirm.js
+++ www/js/endbildschirm.js
@@ -6,7 +6,7 @@ function PunkteAusgabe(){
 		var spieler = obj.name;
 		var letzerVersuch;
 		var erstes_mal_in_schleife = 0;
-		var image;
+		var bild=new Image();
 		
 		var set_statistik_tabelle = localStorage.getItem("statistik");
 		statistik_tabelle = JSON.parse(set_statistik_tabelle);
@@ -21,7 +21,17 @@ function PunkteAusgabe(){
 			}
 		}
 		
+		if(letzerVersuch == 9){
+ 	 	bild.src="img/medal/medal1.png";
+		}
+		if(letzerVersuch == 10 || letzerVersuch == 11){
+ 	 	bild.src="img/medal/medal2.png";
+		}
+		if(letzerVersuch > 11){
+ 	 	bild.src="img/medal/medal3.png";
+		}
+				
+ 	 	document.write('<img src="'+bild.src+'" border="0" width="50%" height="10%">');
 		document.getElementById("letzterVersuch").innerHTML = letzerVersuch;
-		
 		
 }
