f798d685832b82d392c34db823b7f0cd9d591f67
SonjaWallner <Sonja_DanielaW@gmx.at>
2017-03-29 14:46:43 +0200

    LoeschenButtonProgrammierung

--- www/js/statistik.js
+++ www/js/statistik.js
@@ -56,5 +56,46 @@ function statistikWerte(){
 					}
 			}
 		}
+}
+
+function datenloeschen(){
+		var einstellungen_herausholen = localStorage.getItem("einstellungen");
+		obj = JSON.parse(einstellungen_herausholen);
+		var bildset = obj.set;
+		var spieler = obj.name;
+		var erstes_mal_in_schleife = 0;
 		
+		var set_statistik_tabelle = localStorage.getItem("statistik");
+		statistik_tabelle = JSON.parse(set_statistik_tabelle);
+		
+		var set_selected_spielerstatus = localStorage.getItem("spielerstatus");
+		selected_status = JSON.parse(set_selected_spielerstatus);
+					
+		var loeschen=confirm("Daten wirklich entfernen?");
+		if (loeschen == true){
+			for(b = 0; b < statistik_tabelle.names.length; b++){
+			if(statistik_tabelle.names[b] == spieler && statistik_tabelle.sets[b] == bildset){
+					if(erstes_mal_in_schleife == 0){
+					statistik_tabelle.spielpunkte[b] = 0;
+					statistik_tabelle.spielpunkte[b+1] = 0;
+					statistik_tabelle.spielpunkte[b+2] = 0;
+					statistik_tabelle.spielpunkte[b+3] = 0;
+					statistik_tabelle.spielpunkte[b+4] = 0;
+					erstes_mal_in_schleife = 1;
+					}
+			}
+			}
+			
+			for(i=0;i<selected_status.selected.length; i++){
+			if(selected_status.selected[i]=="true"){
+					selected_status.besterVersuch[i] = 0;
+			}
+			}
+			alert("Daten wurden erfolgreich entfernt");
+			select_loeschen = JSON.stringify(statistik_tabelle);
+			localStorage.setItem("statistik", select_loeschen);
+			select_versuch = JSON.stringify(selected_status);
+			localStorage.setItem("spielerstatus", select_versuch);
+			window.document.location.href = "index.html";
+		}
 }
